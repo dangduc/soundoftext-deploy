e30c97a74c478f4068cf0b08409c7f11b756e479
Nick Pierson <me@nick.exposed>
2018-10-25 05:59:04 +0000

    add traefik

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
+MACHINE_NAME=soundoftext
+
 pushd machine
-machine-import soundoftext.zip
+machine-import $MACHINE_NAME.zip
 popd
 
-eval $(docker-machine env soundoftext)
+set -e
+
+docker-machine ssh $MACHINE_NAME "mkdir -p /opt/traefik"
+docker-machine scp ./traefik.toml $MACHINE_NAME:/opt/traefik/traefik.toml
+
+eval $(docker-machine env $MACHINE_NAME)
 
 ./scripts/up.sh
