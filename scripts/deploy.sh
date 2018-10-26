3ae55b8e5ff0f15ce15a1a2a67dbdd6e3acd9a74
Nick Pierson <me@nick.exposed>
2018-10-26 02:29:55 +0000

    add https to traefik for api

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,11 @@ popd
 set -e
 
 docker-machine ssh $MACHINE_NAME "mkdir -p /opt/traefik"
+
 docker-machine scp ./traefik.toml $MACHINE_NAME:/opt/traefik/traefik.toml
+docker-machine scp ./acme.json $MACHINE_NAME:/opt/traefik/acme.json
+
+docker-machine ssh $MACHINE_NAME "chmod 600 /opt/traefik/acme.json"
 
 eval $(docker-machine env $MACHINE_NAME)
 
