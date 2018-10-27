0d2590bbec6137cdf45656e76f183f8b74d98fe7
Nick Pierson <me@nick.exposed>
2018-10-27 19:31:56 +0000

    add start/stop scripts

--- scripts/deploy-run.sh
+++ scripts/deploy-run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+command=${1?Please pass in a run script.}
+
 pushd machine
 machine-import soundoftext.zip
 popd
 
 eval $(docker-machine env soundoftext)
 
-./scripts/down.sh
+"$command"
